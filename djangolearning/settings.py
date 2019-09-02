9dd79f14f9c202fc4d05c546c22b5c3e33434273
Bilal <bilaluddin474@outlook.com>
2019-09-02 08:51:20 +0100

    Image Fix

--- djangolearning/settings.py
+++ djangolearning/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
